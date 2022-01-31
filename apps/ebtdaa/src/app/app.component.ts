2ca1061aeb759117d017f5b27f7e6d81a262a693
Hozefa Mangalorewala <hozefam@live.in>
2022-01-31 13:51:54 +0300

    Added scrolling on Navigation

--- apps/ebtdaa/src/app/app.component.ts
+++ apps/ebtdaa/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 import { LocalStorage } from '@ngx-pwa/local-storage';
-import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 import defaultLanguage from '../assets/i18n/ar.json';
 
 @Component({
@@ -15,11 +17,20 @@ export class AppComponent implements OnInit {
 
   constructor(
     private translate: TranslateService,
-    protected localStorage: LocalStorage
+    protected localStorage: LocalStorage,
+    private router: Router,
+    private viewportScroller: ViewportScroller
   ) {
     translate.setTranslation(this.selectedLang, defaultLanguage);
     translate.setDefaultLang(this.selectedLang);
     translate.use(this.selectedLang);
+
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scroll(0, 0);
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
   }
 
   ngOnInit(): void {}
